a5bd6ecc599dc0a132cac6f58a6f0ed484005f58
Hexagon <hexagon@56k.guru>
2024-03-12 01:09:48 +0100

    Implement @cross/test. Update deps.

--- tests/mod.test.ts
+++ tests/mod.test.ts
@@ -11,170 +11,110 @@ import {
     validateEnv,
     ValidatorFunction,
 } from "../mod.ts";
+import { test } from "@cross/test";
 
 /** ==== getEnv() ==== */
-Deno.test({
-    name: "getEnv() retrieves an existing environment variable",
-    fn() {
-        Deno.env.set("TEST_VARIABLE", "hello");
+test("getEnv() retrieves an existing environment variable", () => {
+    setEnv("TEST_VARIABLE", "hello");
 
-        const value = getEnv("TEST_VARIABLE");
-        assertEquals(value, "hello");
-
-        Deno.env.delete("TEST_VARIABLE");
-    },
+    const value = getEnv("TEST_VARIABLE");
+    assertEquals(value, "hello");
 });
 
-Deno.test({
-    name: "getEnv() undefined for nonexistant keys",
-    fn: () => {
-        const result = getEnv("TEST_KEY_NONEXISTANT");
-        assertEquals(result, undefined);
-    },
+test("getEnv() undefined for nonexistant keys", () => {
+    const result = getEnv("TEST_KEY_NONEXISTANT");
+    assertEquals(result, undefined);
 });
 
 /** ==== requireEnv() ==== */
-Deno.test({
-    name: "requireEnv() retrieves an existing environment variable",
-    fn() {
-        Deno.env.set("TEST_VARIABLE", "hello");
+test("requireEnv() retrieves an existing environment variable", () => {
+    setEnv("TEST_VARIABLE_2", "hello");
 
-        const value = requireEnv("TEST_VARIABLE");
-        assertEquals(value, "hello");
-
-        Deno.env.delete("TEST_VARIABLE");
-    },
+    const value = requireEnv("TEST_VARIABLE_2");
+    assertEquals(value, "hello");
 });
 
-Deno.test({
-    name: "requireEnv() throws for nonexistant keys",
-    fn: () => {
-        assertThrows(() => requireEnv("TEST_KEY_NONEXISTANT"), "TEST_KEY_NONEXISTANT not defined in environment.");
-    },
+test("requireEnv() throws for nonexistant keys", () => {
+    assertThrows(() => requireEnv("TEST_KEY_NONEXISTANT"), "TEST_KEY_NONEXISTANT not defined in environment.");
 });
 
 /** ==== getAllEnv() ==== */
-Deno.test({
-    name: "getAllEnv() with prefix filters variables",
-    fn: () => {
-        Deno.env.set("API_KEY", "12345");
-        Deno.env.set("API_VERSION", "v2");
-        Deno.env.set("OTHER_VAR", "test");
+test("getAllEnv() with prefix filters variables", () => {
+    setEnv("API_KEY", "12345");
+    setEnv("API_VERSION", "v2");
+    setEnv("OTHER_VAR", "test");
 
-        const apiVariables = getAllEnv("API_");
+    const apiVariables = getAllEnv("API_");
 
-        assertEquals(apiVariables, {
-            API_KEY: "12345",
-            API_VERSION: "v2",
-        });
-
-        Deno.env.delete("API_KEY");
-        Deno.env.delete("API_VERSION");
-        Deno.env.delete("OTHER_VAR");
-    },
+    assertEquals(apiVariables, {
+        API_KEY: "12345",
+        API_VERSION: "v2",
+    });
 });
 
 /** ==== setEnv() ==== */
-Deno.test({
-    name: "setEnv() creates a new environment variable",
-    fn: () => {
-        const key = "TEST_VARIABLE";
-        const value = "test_value";
+test("setEnv() creates a new environment variable", () => {
+    const key = "TEST_VARIABLE_3";
+    const value = "test_value";
 
-        setEnv(key, value);
-        assertEquals(Deno.env.get(key), value);
-
-        Deno.env.delete(key);
-    },
+    setEnv(key, value);
+    assertEquals(getEnv(key), value);
 });
 
-Deno.test({
-    name: "setEnv() overwrites an existing environment variable",
-    fn: () => {
-        const key = "TEST_VARIABLE";
-        Deno.env.set(key, "old_value");
+test("setEnv() overwrites an existing environment variable", () => {
+    const key = "TEST_VARIABLE_4";
+    setEnv(key, "old_value");
 
-        setEnv(key, "new_value");
-        assertEquals(Deno.env.get(key), "new_value");
-
-        Deno.env.delete(key);
-    },
+    setEnv(key, "new_value");
+    assertEquals(getEnv(key), "new_value");
 });
 
 /** ==== hasEnv() ==== */
-Deno.test({
-    name: "hasEnv() returns true for an existing environment variable",
-    fn: () => {
-        const key = "TEST_VARIABLE";
-        Deno.env.set(key, "value");
+test("hasEnv() returns true for an existing environment variable", () => {
+    const key = "TEST_VARIABLE_5";
+    setEnv(key, "value");
 
-        assert(hasEnv(key));
-
-        Deno.env.delete(key);
-    },
+    assert(hasEnv(key));
 });
 
-Deno.test({
-    name: "hasEnv() returns false for a non-existent environment variable",
-    fn: () => {
-        const key = "NON_EXISTENT_VARIABLE";
-        assertEquals(hasEnv(key), false);
-    },
+test("hasEnv() returns false for a non-existent environment variable", () => {
+    const key = "NON_EXISTENT_VARIABLE";
+    assertEquals(hasEnv(key), false);
 });
 
 /** ==== validateEnv() ==== */
 const isValidPort: ValidatorFunction = (value: string): boolean => /^\d+$/.test(value);
 
-Deno.test({
-    name: "validateEnv() returns true for valid values",
-    fn: () => {
-        Deno.env.set("TEST_VALUE", "1234"); // Valid as per isValidPort
-        assert(validateEnv("TEST_VALUE", isValidPort));
-        Deno.env.delete("TEST_VALUE");
-    },
+test("validateEnv() returns true for valid values", () => {
+    setEnv("TEST_VALUE_2", "1234"); // Valid as per isValidPort
+    assert(validateEnv("TEST_VALUE_2", isValidPort));
+    Deno.env.delete("TEST_VALUE_2");
 });
 
-Deno.test({
-    name: "validateEnv() false for nonexistant keys",
-    fn: () => {
-        const result = validateEnv("TEST_VALUE_NONEXISTANT", isValidPort);
-        assertEquals(result, false);
-    },
+test("validateEnv() false for nonexistant keys", () => {
+    const result = validateEnv("TEST_VALUE_NONEXISTANT", isValidPort);
+    assertEquals(result, false);
 });
 
 /** ==== validateAndGetEnv() ==== */
-Deno.test({
-    name: "validateAndGetEnv() returns the value for valid values",
-    fn: () => {
-        Deno.env.set("TEST_PORT", "8080");
-        const port = validateAndGetEnv("TEST_PORT", isValidPort);
-        assertEquals(port, "8080");
-        Deno.env.delete("TEST_PORT");
-    },
+test("validateAndGetEnv() returns the value for valid values", () => {
+    setEnv("TEST_PORT", "8080");
+    const port = validateAndGetEnv("TEST_PORT", isValidPort);
+    assertEquals(port, "8080");
 });
 
-Deno.test({
-    name: "validateAndGetEnv() returns undefined for invalid values",
-    fn: () => {
-        Deno.env.set("PORT", "http"); // Invalid as per isValidPort
+test("validateAndGetEnv() returns undefined for invalid values", () => {
+    setEnv("PORT", "http"); // Invalid as per isValidPort
 
-        // should return undefined
-        const result = validateAndGetEnv("PORT", isValidPort);
-        assertEquals(result, undefined);
-
-        Deno.env.delete("PORT");
-    },
+    // should return undefined
+    const result = validateAndGetEnv("PORT", isValidPort);
+    assertEquals(result, undefined);
 });
 
-Deno.test({
-    name: "validateAndGetEnv() throws for invalid values",
-    fn: () => {
-        Deno.env.set("PORT", "http"); // Invalid as per isValidPort
+test("validateAndGetEnv() throws for invalid values", () => {
+    setEnv("PORT", "http"); // Invalid as per isValidPort
 
-        setupEnv({ throwErrors: true });
-        //it should throw an error:
-        assertThrows(() => validateAndGetEnv("PORT", isValidPort), ValidationError);
-
-        Deno.env.delete("PORT");
-    },
+    setupEnv({ throwErrors: true });
+    //it should throw an error:
+    assertThrows(() => validateAndGetEnv("PORT", isValidPort), ValidationError);
 });
